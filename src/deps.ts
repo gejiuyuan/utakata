38b8f2f76bb325e56b1b2292601d10717ef73d41
gejiuyuan <2507188687@qq.com>
2021-12-08 23:22:50 +0800

    增加track追踪前提条件；增加代码注释

--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,9 @@ import { activeEffect, ReactiveEffect } from "./effect";
 import { PlainObject } from "./typing";
 import { hasChanged } from "./utils";
 
+/**
+ * 响应式数据收集的依赖集合
+ */
 export const objEffectWeakMap = new WeakMap<
   object,
   Map<
@@ -10,7 +13,27 @@ export const objEffectWeakMap = new WeakMap<
   >
 >();
 
+/**
+ * 是否应该暂停追踪依赖
+ * @returns 
+ * @description
+ *  不应该收集的条件：
+ *    1、activeEffect为undefined
+ */
+export function isPauseTracking() {
+  return activeEffect === void 0;
+}
+
+/**
+ * 响应式数据依赖收集
+ * @param target 
+ * @param key 
+ * @returns 
+ */
 export function track(target: PlainObject, key: string | symbol) {
+  if (isPauseTracking()) {
+    return;
+  }
   let effectDepMap = objEffectWeakMap.get(target);
   if (!effectDepMap) {
     objEffectWeakMap.set(target, effectDepMap = new Map());
@@ -21,9 +44,18 @@ export function track(target: PlainObject, key: string | symbol) {
   } else {
     effectFuncDeps.add(activeEffect!);
   }
+  // 同时建立响应式数据依赖与activeEffect的双向映射
   activeEffect!.deps.push(effectFuncDeps);
 }
 
+/**
+ * 触发响应式数据更新时的任务
+ * @param target 
+ * @param key 
+ * @param newValue 
+ * @param oldValue 
+ * @returns 
+ */
 export function trigger(target: PlainObject, key: string | symbol, newValue: any, oldValue: any) {
   const targetDepMap = objEffectWeakMap.get(target);
   if (!targetDepMap) {
@@ -35,6 +67,7 @@ export function trigger(target: PlainObject, key: string | symbol, newValue: any
   }
   if (hasChanged(newValue, oldValue)) {
     Array.from(targetEffectFuncSet).forEach(effectFunc => {
+      // 优先执行调度器任务，其次是基础获取任务
       effectFunc.schduler?.() || effectFunc.run();
     });
   }
--- src/effect.ts
+++ src/effect.ts
@@ -9,6 +9,12 @@ export type ReactiveEffectFunc = CommonFunc & {
   effect?: ReactiveEffect;
 }
 export class ReactiveEffect {
+  /**
+   * 清空effect副作用对象的deps依赖项
+   * @param effect 
+   * @description
+   *  用以终止响应式监听器
+   */
   static cleanUpEffect(effect: InstanceType<typeof ReactiveEffect>) {
     const { deps } = effect;
     deps.forEach((dep) => {
@@ -16,12 +22,25 @@ export class ReactiveEffect {
     });
     deps.length = 0;
   }
+  /**
+   * effect副作用对象的deps依赖项
+   */
   public deps: Set<ReactiveEffect>[] = [];
   constructor(
+    /**
+     * effect、watch等API的回调（侦听处理器）
+     */
     public func: ReactiveEffectFunc,
+    /**
+     * 异步调度器（优先级高于run）
+     */
     public schduler: CommonFunc | null = null
   ) { }
 
+  /**
+   * 侦听器执行者
+   * @returns 
+   */
   run() {
     if (!effectStack.includes(this)) {
       try {
@@ -34,6 +53,9 @@ export class ReactiveEffect {
       }
     }
   }
+  /**
+   * 终止侦听器
+   */
   stop() {
     ReactiveEffect.cleanUpEffect(this);
   }
@@ -56,47 +78,90 @@ export function effect(
   return runner;
 }
 
+/**
+ * 侦听器触发时机
+ */
 export enum WatcherFlush {
+  /**
+   * 异步
+   */
   async = 'async',
+  /**
+   * 同步
+   */
   sync = 'sync',
 }
 const watcherFlushValues = Object.values(WatcherFlush);
+
+/**
+ * watch api执行选项
+ */
 export interface WatcherOptions {
+  /**
+   * 刷新时机（触发时机）
+   */
   flush?: WatcherFlush;
+  /**
+   * 是否立即执行
+   */
   immediate?: boolean;
 }
 
+/**
+ * watch侦听器默认返回的初始值
+ */
 const INITIAL_WATCHER_VALUE = {}
+
+/**
+ * watch响应式侦听器
+ * @param targetSource 获取要侦听的响应式数据
+ * @param cb 响应式数据更新时，需要处理的任务
+ * @param options 执行选项
+ * @returns 
+ */
 export function watch(
   targetSource: () => ReactiveIdentification | ReactiveIdentification[],
   cb: CommonFunc,
   options: WatcherOptions = EMPTY_OBJECT
 ) {
+  // 如果cb不是函数，终止后续执行
   if (!isFunc(cb)) {
     console.error(`The second parameter of 'watch' ———— 'cb' must be a function!`)
     return;
   }
+  // 默认触发机制：响应式数据更新后，异步执行回调任务
   const { flush = WatcherFlush.async, immediate = false } = options;
+  // 获取响应式数据的值
   const targetValueGetter = () => targetSource();
+  // 旧值
   let oldValue = INITIAL_WATCHER_VALUE;
+  // 侦听器的核心处理任务
   const baseJob = () => {
+    // 获取响应式数据更新后的值
     const newValue = _effect.run();
+    // 如果新值与旧值不同，就执行回调任务
     if (hasChanged(newValue, oldValue)) {
       cb.apply(null, [newValue, oldValue === INITIAL_WATCHER_VALUE ? void 0 : oldValue]);
+      // 同时将新值为旧值
       oldValue = newValue;
     }
   }
   if (!watcherFlushValues.includes(flush)) {
     console.warn(`The 'flush' parameter value of 'watch' options should be one of the two options: ${watcherFlushValues.join('、')}`)
   }
+  // 调度器（可能是异步，也可能是同步）
   const schduler = flush === WatcherFlush.async ? () => nextTick(baseJob) : baseJob;
   const _effect = new ReactiveEffect(targetValueGetter, schduler);
 
+  // 如果需要立即执行
   if (immediate) {
     schduler();
-  } else {
+  }
+  // 否则先获取一次旧值
+  else {
     oldValue = _effect.run();
   }
+  // 返回侦听器中断者
   return () => {
     _effect.stop();
   }
